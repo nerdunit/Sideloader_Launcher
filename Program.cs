8e134e6dd92b62f18165efff7df5e834efba619c
nerdunit <60826155+nerdunit@users.noreply.github.com>
2020-12-04 13:34:35 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System;
+
+
+namespace Sideloader_Launcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles(Environment.CurrentDirectory);
+            foreach (string file in files)
+            {
+                string fileName = file;
+                while (fileName.Contains("\\"))
+                {
+                    fileName = fileName.Substring(fileName.IndexOf("\\") + 1);
+                }
+                if (fileName.StartsWith("Android") && fileName.EndsWith(".exe") && !fileName.Contains("Launcher"))
+                {
+                    System.Diagnostics.Process.Start(file);
+                }
+            }
+        }
+    }
+}
